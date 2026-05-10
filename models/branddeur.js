5b6c5660581791d3896907b00fc7218215a579c5
aikobeyers <aikobeyers@hotmail.com>
2026-05-10 21:35:59 +0200

    Update to esmodule

--- models/branddeur.js
+++ models/branddeur.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const branddeurSchema = new mongoose.Schema({
     name: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Branddeur', branddeurSchema);
+export default mongoose.model('Branddeur', branddeurSchema);

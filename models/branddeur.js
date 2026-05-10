e058dd9f1fceb4ce1fe762e907d4e9ae8baae44e
aikobeyers <aikobeyers@hotmail.com>
2026-05-10 22:54:45 +0200

    Update model again

--- models/branddeur.js
+++ models/branddeur.js
@@ -2,6 +2,14 @@ import mongoose from 'mongoose';
 
 const branddeurSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    status: { type: String },
+    doorType: { type: String },
+    resistanceMinutes: { type: Number },
+    building: { type: String },
+    floor: { type: String },
+    location: { type: String },
+    nextInspectionDate: { type: Date },
+    manufacturer: { type: String },
 });
 
 export default mongoose.model('Branddeur', branddeurSchema, 'branddeuren');

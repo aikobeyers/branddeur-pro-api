149a2b3fdf886faec59f9af3e061fb727cc65ba2
aikobeyers <aikobeyers@hotmail.com>
2026-05-10 22:19:45 +0200

    Update collection name

--- models/branddeur.js
+++ models/branddeur.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const branddeurSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
 });
 
-export default mongoose.model('Branddeur', branddeurSchema);
+export default mongoose.model('Branddeur', branddeurSchema, 'branddeuren');
